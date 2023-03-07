1b8aa7978100c3d346136838cb5dddfb5ace83d4
MeryemAjjane <meryem.ajjane@uit.ac.ma>
2023-03-07 20:40:12 +0100

    1 eme mdification

---
+++ src/main/java/Calcul.java
@@ -0,0 +1,8 @@
+
+
+public class Calcul {
+	public double add(int a,int b) {
+		return a+b;
+	}
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+
+
+public class main {
+
+	public static void main(String[] args) {
+		Calcul calcul=new Calcul() ;
+		double c = calcul.add(4, 6);
+		System.out.println(c);
+
+	}
+
+}
